8c1a7bb53856da5ee39cca51c80a2b584a83585a
RowenaMacaraeg <ro@bangmac.com>
2022-01-27 19:34:51 -0800

    Adding React Router Routes & Navigation

--- src/React/Layout/Main.jsx
+++ src/React/Layout/Main.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Switch, Route } from 'react-router-dom';
 
 import Homework from '../Homework/Homework.jsx';
+import Welcome from '../Welcome/Welcome.jsx'; 
+import Services from '../Services/Services.jsx'; 
+import Contact from '../Contact/Contact.jsx';
+import Login from '../Login/Login.jsx';  
+
 const Main = () => {
 
     return (
-        <MainStyled className='Main'>
-            Main 
-            <Homework />
+         <MainStyled className='Main'>
+            <Switch>
+                <Route path='/'exact>
+                    <Welcome />
+                </Route>
+    
+                <Route path='/services'>
+                    <Services />
+                </Route>
+
+                <Route path='/contact'>
+                    <Contact />
+                </Route>
+
+                <Route path='/login'>
+                    <Login />
+                </Route>
+
+                <Route path='/homework'>
+                    <Homework />
+                </Route>
+            </Switch>
         </MainStyled>
     );
 }

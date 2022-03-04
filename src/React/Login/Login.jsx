be62d371d59db992c7c312042af17f1840e2f429
Rowena Macaraeg <ro@ripbang.com>
2022-03-03 18:24:40 -0800

    Login Form Challenge

--- src/React/Login/Login.jsx
+++ src/React/Login/Login.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/* Components ---------------------------*/
+import UniversalForm, { Input, Textarea, SubmitButton } from 'React/common/UniversalForm/UniversalForm.jsx';
+
 const Login = () => {
 
-    
+    const onSubmitHandler = ({fields, payload}) => {
+        console.log('onSubmit fields, payload', fields, payload);
+    }
     
     return (
         <LoginStyled className='Login'>
-        <h1>    Login </h1>
+            <h1>Login </h1>
+            <UniversalForm
+                apiUrl='/user/login'
+                onSubmit={ onSubmitHandler }
+            >
+                
+                <Input
+                    label = 'Username'
+                    id='username'
+                    placeholder='user@domain.com'
+                    rules={ ['required', 'email'] }
+                />
+
+                 <Input
+                    label = 'Password'
+                    id='password'
+                    rules={ ['required']}
+                />
+
+                <SubmitButton> Log In </SubmitButton>
+            </UniversalForm>
         </LoginStyled>
     );
 }

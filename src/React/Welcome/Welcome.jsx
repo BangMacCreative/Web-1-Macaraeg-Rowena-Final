f483f0d06bc6fa8c3e015a6843c7d3bc132cc35b
Rowena Macaraeg <ro@ripbang.com>
2022-03-07 18:45:53 -0800

    Slideshow: Adding images and Data

--- src/React/Welcome/Welcome.jsx
+++ src/React/Welcome/Welcome.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import Slideshow from './Slideshow/Slideshow.jsx';
 import Tabbed from './Tabbed/Tabbed.jsx';
 import  Staff from './Staff/Staff.jsx';
+
 
 const Welcome = () => {
 
     return (
         <WelcomeStyled className='Welcome'>
             <h1>Welcome </h1>
+            <Slideshow/>
             <Tabbed />
             <Staff />
         </WelcomeStyled>

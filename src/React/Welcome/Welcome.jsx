872a7d3afbfd981ebe4cd45ceb1c87c9263149c5
Rowena Macaraeg <ro@ripbang.com>
2022-03-14 19:11:54 -0700

    polishing type

--- src/React/Welcome/Welcome.jsx
+++ src/React/Welcome/Welcome.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/* Components ---------------------------*/
 import Slideshow from './Slideshow/Slideshow.jsx';
 import About from './About/About.jsx';
 import Tabbed from './Tabbed/Tabbed.jsx';
-import Staff from './Staff/Staff.jsx';
+import UpButton from './UpButton/UpButton.jsx';
+// import Staff from './Staff/Staff.jsx';
 // import Projects from './Projects/Projects.jsx';
-// import  Staff from './Staff/Staff.jsx';
+
 
 const Welcome = () => {
 
@@ -14,7 +16,8 @@ const Welcome = () => {
         <WelcomeStyled className='Welcome'>
             <Slideshow/>
             <About/>
-             <Tabbed/>
+            <Tabbed/>
+            <UpButton/>
             {/* <Projects/> */}
             {/* <Staff/> */}
         </WelcomeStyled>
